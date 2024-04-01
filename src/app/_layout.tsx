f67dd035a940d6dc5a630f1a4aa90df6f1c0c68f
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-04-01 20:51:23 -0300

    fix: fix styles and configs from tailwind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
 import "@styles/global.css";
 
-import { Slot } from "expo-router";
-
 export default function Layout() {
-    return <Slot />;
+    return (
+        <>
+            <StatusBar style='light' />
+            <Slot />
+        </>
+    );
 }
