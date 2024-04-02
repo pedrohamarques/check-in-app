b8fae1bdfa8a6e10bbb088a5abfe8b053926b5b7
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-04-01 21:02:01 -0300

    feat: add loading component to layout when loading fonts

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = function (api) {
                         "@assets": "./src/assets",
                         "@styles": "./src/styles",
                         "@typings": "./src/typings",
+                        "@components": "./src/components",
                     },
                 },
             ],
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,9 +2,28 @@ import React from "react";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
+import {
+    useFonts,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { Loading } from "@components/loading";
+
 import "@styles/global.css";
 
 export default function Layout() {
+    const [isFontsLoaded] = useFonts({
+        Roboto_400Regular,
+        Roboto_500Medium,
+        Roboto_700Bold,
+    });
+
+    if (!isFontsLoaded) {
+        return <Loading testID='app._layout.loading' />;
+    }
+
     return (
         <>
             <StatusBar style='light' />
